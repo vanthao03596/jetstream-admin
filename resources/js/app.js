9d8487c5822112f4ce5b5f8c51b1d71c32981cf0
phamthao <phamthao03596@gmail.com>
2021-05-11 09:41:04 +0700

    chore: add tonysm/turbo-laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,15 @@
-require('./bootstrap');
+import * as Turbo from '@hotwired/turbo';
+import('./bootstrap');
+import('./elements/turbo-echo-stream-tag');
+import 'alpine-turbo-drive-adapter';
+import 'alpinejs';
 
-require('alpinejs');
+import { Application } from 'stimulus'
+import { definitionsFromContext } from '@stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+
+Turbo.start();
